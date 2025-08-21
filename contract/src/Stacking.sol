109172efd07bf87f756b589df1d5a2a1210ab692
Nikunj <mnikunj262@gmail.com>
2025-08-21 22:58:45 +0530

    update: staking contract

--- contract/src/Stacking.sol
+++ contract/src/Stacking.sol
@@ -2,7 +2,59 @@
 pragma solidity ^0.8.13;
 
 contract StakingContract {
+
+    uint256 totalBalance;
+    mapping(address => uint256) balances;
+    mapping(address => uint) unclaimedRewards;
+    mapping(address => uint) lastUpdateTime;
+
     constructor() {
 
+    }
+
+    function stake() public payable {
+        require(msg.value > 0); 
+
+        if(lastUpdateTime[msg.sender] != 0) {
+            lastUpdateTime[msg.sender]  = block.timestamp;
+        } else {
+            unclaimedRewards[msg.sender]  += (block.timestamp - lastUpdateTime[msg.sender]) * balances[msg.sender];
+            lastUpdateTime[msg.sender] = block.timestamp;
+        }
+        balances[msg.sender] += msg.value;
+    }
+
+    function unstake(uint _amount) public {
+        require(balances[msg.sender] >= _amount);
+
+        unclaimedRewards[msg.sender]  += (block.timestamp - lastUpdateTime[msg.sender]) * balances[msg.sender];
+        lastUpdateTime[msg.sender] = block.timestamp;
+        
+        payable(msg.sender).transfer(_amount);
+        balances[msg.sender] -= _amount;
+    }
+
+
+    function getRewards(address _address) public view returns (uint) {
+        uint currnetReward = unclaimedRewards[_address];
+        uint updateTime = lastUpdateTime[_address];
+        uint newReward =(block.timestamp - updateTime) * balances[_address];
+        return currnetReward + newReward;
+    }
+
+
+    function claimRewards() public {
+        uint256 currnetReward = unclaimedRewards[msg.sender];
+        uint256 updateTime = lastUpdateTime[msg.sender];
+        uint256 newReward =(block.timestamp - updateTime) * balances[msg.sender];
+
+        // currnet + new Reward send to msg.sender
+
+        unclaimedRewards[msg.sender] = 0;
+        lastUpdateTime[msg.sender] = block.timestamp;
+    } 
+
+    function balanceOf(address _address) public view returns (uint256) {
+        return balances[_address];
     }
 }
