fa208f53e638a811cdc42fba0e54c8b8e600f1f6
Nikunj <mnikunj262@gmail.com>
2025-08-21 22:58:24 +0530

    update: orca contract

--- contract/src/Orca.sol
+++ contract/src/Orca.sol
@@ -2,9 +2,23 @@
 pragma solidity ^0.8.13;
 
 import { ERC20 } from '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import { Ownable } from '@openzeppelin/contracts/access/Ownable.sol';
 
-contract OrcaContract is ERC20 {
-    constructor() ERC20("Orca", "ORC") {
+contract OrcaContract is ERC20, Ownable {
 
+    address stakingContract;
+    
+    constructor() ERC20("Orca", "ORC")  Ownable(msg.sender) {
+
+    }
+
+    
+    function mint(address _account,uint256 _amount) public {
+        require(msg.sender == stakingContract);
+        _mint(_account, _amount);
+    }
+
+    function updateStaking(address _stakingContract) public onlyOwner {
+        stakingContract = _stakingContract;
     }
 }
