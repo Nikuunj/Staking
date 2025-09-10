91cd65a7a66bcc48a6db31eb122a6b8c4160a3ed
Nikunj <mnikunj262@gmail.com>
2025-09-10 11:09:16 +0530

    update: balance

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,18 +1,24 @@
 "use client"
 import { Connectors } from "@/components/ui/Connectors";
 import { Disconnect } from "@/components/ui/Disconnect";
-import { useAccount } from "wagmi";
+import { useAccount, useBalance } from "wagmi";
 
 
 export default function Home() {
     const { address } = useAccount()
+
+    const balance = useBalance({
+        address
+    })
 
     return (
         <div>
             <div>
                 {!address ? <Connectors /> : <Disconnect />}
             </div>
-            {address}
+            {address} 
+            <br />
+            {balance.data?.formatted}
         </div>
     );
 }
