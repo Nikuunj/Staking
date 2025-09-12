05ffe1ea3bd6721cadc46e93a5a4879aeb3fd3cf
Nikunj <mnikunj262@gmail.com>
2025-09-12 19:12:32 +0530

    update: how it work complete

--- client/components/home/HowToStacke.tsx
+++ client/components/home/HowToStacke.tsx
@@ -1,3 +1,4 @@
+import { ArrowDownCircleIcon, CoinsIcon, LogInIcon, Wallet2Icon } from "lucide-react"
 import BoxWork from "../ui/BoxWork"
 import ShiningText from "../ui/ShiningText"
 
@@ -8,11 +9,28 @@ function HowToStacke() {
             How {' '} <ShiningText text="stake" /> {' '} Ethereum ? 
          </h1>
 
-         <div>
-            {/* <BoxWork /> */}
-            {/* <BoxWork /> */}
-            {/* <BoxWork /> */}
-            <BoxWork /> 
+         <div className="flex flex-wrap gap-10 max-w-2xl justify-center">
+            <BoxWork 
+               header="Connect Wallet"
+               icon={<LogInIcon className="text-blue-500 drop-shadow-[0_0_6px_rgba(59,130,246,0.6)]" />}
+               describe="Start by securely connecting your Ethereum wallet to our platform."
+
+            /> 
+            <BoxWork 
+               header="Stake ETH with Us"
+               icon={<Wallet2Icon className="text-purple-500 drop-shadow-[0_0_6px_rgba(168,85,247,0.6)]" />}
+               describe="Deposit your ETH into our staking system to begin earning."
+            /> 
+            <BoxWork 
+               header="Earn Rewards"
+               icon={<CoinsIcon className="text-yellow-500 drop-shadow-[0_0_6px_rgba(234,179,8,0.6)]" />}
+               describe="Receive staking rewards based on the amount of ETH you staked."
+            /> 
+            <BoxWork 
+               header="Unstake Anytime"
+               icon={<ArrowDownCircleIcon className="text-green-500 drop-shadow-[0_0_6px_rgba(34,197,94,0.6)]" />}
+               describe="You're in control—unstake your ETH whenever you want, no lock-in."
+            /> 
          </div>
       </section>
    )
--- client/components/ui/BoxWork.tsx
+++ client/components/ui/BoxWork.tsx
@@ -8,7 +8,14 @@ interface BoxWorkProps {
 
 function BoxWork({ header, describe, icon }: BoxWorkProps) {
    return (
-      <div>BoxWork</div>
+      <div className={'border max-w-64 py-13 px-11 space-y-6 rounded-3xl  border-teal-900'}>
+         <div className="flex gap-2 items-center">
+            {icon} {header}
+         </div>
+         <div className="break-words text-balance text-zinc-300/90">
+            {describe}
+         </div>
+      </div>
    )
 }
 
