28935fbbbdc2052bf1fc94243b4c403c4c0f5103
Nikunj <mnikunj262@gmail.com>
2025-09-15 20:53:33 +0530

    update: disconnect and connect fn

--- client/components/ui/SlideBar.tsx
+++ client/components/ui/SlideBar.tsx
@@ -1,15 +1,20 @@
-import { ChevronLeft, MoveLeftIcon } from "lucide-react"
+"use client"
+import { ChevronLeft, PowerIcon, PowerOffIcon } from "lucide-react"
 import Link from "next/link"
 import { Dispatch, SetStateAction } from "react"
 import Button from "./Button"
+import { Disconnect } from "./Disconnect"
+import { useAccount } from "wagmi"
+import ConnectBtn from "./ConnectBtn"
 
 function SlideBar({ closeOpen }: { closeOpen: Dispatch<SetStateAction<boolean>> }) {
+   const { address }  = useAccount();
    return (
       <div className="fixed inset-0 grid grid-cols-5">
-         <div className="bg-black/15 border-r border-emerald-800 w-full hidden sm:block col-span-3 lg:col-span-4" onClick={() => closeOpen(false)}>
+         <div className="bg-black/30 border-r border-emerald-800 w-full hidden sm:block col-span-3 lg:col-span-4" onClick={() => closeOpen(false)}>
             
          </div>
-         <div className="bg-black/35 w-full py-16 px-10 flex flex-col gap-2 text-zinc-300 col-span-5 sm:col-span-2 lg:col-span-1">
+         <div className="bg-black/65 w-full py-16 px-10 flex flex-col gap-2 text-zinc-300 col-span-5 sm:col-span-2 lg:col-span-1">
             <Button varient='outline' size="sm" className=" absolute top-6 rounded-lg" handleClick={() => closeOpen(false)}>
                <ChevronLeft className="text-zinc-300"/>
             </Button>
@@ -22,7 +27,14 @@ function SlideBar({ closeOpen }: { closeOpen: Dispatch<SetStateAction<boolean>> 
             <Link href={'/rewards'} className="w-fit outline-0">
                <div className="w-fit hover:bg-emerald-800/40 p-2 rounded-lg">Claim Reward</div>
             </Link>
+            <div className="absolute bottom-15">
+               {address ? 
+               <Disconnect className="outline-0"><PowerOffIcon className="text-red-700"/></Disconnect> :
+               <ConnectBtn><PowerIcon className="text-green-700"/></ConnectBtn>
+               }
+            </div>
          </div>
+
       </div>
    )
 }
